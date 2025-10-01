b9417fd5f5a22e209f2cdd463f9b2c7998ece883
Tbelzbby <belloafeez28@gmail.com>
2025-10-01 01:26:51 +0100

    disabl

--- technxxtHub/settings.py
+++ technxxtHub/settings.py
@@ -100,8 +100,8 @@ REST_FRAMEWORK = {
 # JWT Settings
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
-    'ROTATE_REFRESH_TOKENS': False,
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
 }
 
