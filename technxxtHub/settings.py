00a03f23a5cf25e3a00ebb95403f35d04829fd89
Tbelzbby <belloafeez28@gmail.com>
2025-10-02 22:18:22 +0100

    deploying to render

--- technxxtHub/settings.py
+++ technxxtHub/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'web-production-9d0ce.up.railway.app', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'technxxt.onrender.com', 'web-production-9d0ce.up.railway.app', 'localhost']
 
 
 # Application definition
